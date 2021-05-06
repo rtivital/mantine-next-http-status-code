c58060332f47646d2a00aa31595daa5c8eb82085
Резанова Александра <rezanovasasha@gmail.com>
2021-05-06 12:51:00 +0300

    Updates

--- _next/static/webpack/webpack.92b5699e702ce168745a.hot-update.js
+++ _next/static/webpack/webpack.92b5699e702ce168745a.hot-update.js
@@ -12,19 +12,8 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "92b5699e702ce168745a"; }
+/******/ 	__webpack_require__.h = function() { return "825cca891509de5ac971"; }
 /******/ }();
 /******/ 
-/******/ /* webpack/runtime/compat */
-/******/ 
-/******/ 
-/******/ // noop fns to prevent runtime errors during initialization
-/******/ if (typeof self !== "undefined") {
-/******/ 	self.$RefreshReg$ = function () {};
-/******/ 	self.$RefreshSig$ = function () {
-/******/ 		return function (type) {
-/******/ 			return type;
-/******/ 		};
-/******/ 	};
-/******/ }}
+/******/ }
 );
