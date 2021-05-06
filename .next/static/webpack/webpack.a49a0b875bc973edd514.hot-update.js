0de65f6382d819b550f71aac4d19343960e86542
Резанова Александра <rezanovasasha@gmail.com>
2021-05-06 12:50:25 +0300

    Initial commit

--- .next/static/webpack/webpack.a49a0b875bc973edd514.hot-update.js
+++ .next/static/webpack/webpack.a49a0b875bc973edd514.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "104b88d0752660610c0e"; }
+/******/ 	__webpack_require__.h = function() { return "92b5699e702ce168745a"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
